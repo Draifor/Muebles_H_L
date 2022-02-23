3585cfeed0dcf283cf221e2b20e27383d073216a
Alfredo Parra <alfredo-920@hotmail.com>
2022-02-22 22:07:28 -0500

    Se añadió la página agregar cliente y se escribió un poco de js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
+const btnIngresar = document.getElementById("ingresar");
 const btnCerrarSesion = document.getElementById("cerrar-sesion");
+const btnRegresar = document.getElementById("regresar");
 
+// Función para iniciar sesión
+const iniciarSesion = evt => {
+    location.href = "index.html";
+    evt.preventDefault();
+}
+
+// Cerrar la sesión
 btnCerrarSesion.onclick = () => {
-    location.href = "inicio-sesion.html"
+    location.href = "inicio-sesion.html";
+}
+
+// Botón regresar
+btnRegresar.onclick = () => {
+    history.go(-1);
+}
+
+// Función para agregar cliente
+const agregarCliente = evt => {
+    location.href = "clientes.html"
+    evt.preventDefault();
 }
